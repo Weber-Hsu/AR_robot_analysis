9602150a0c8eaa181b8ec4abe5d2cd1ad8c44a87
WeberHsu (Shang Wei) <patric0123@gmail.com>
2016-09-16 16:31:47 +0800

    Firstt commit

---
+++ andbot1dot2/andbot1dot2_modeling_param.m
@@ -0,0 +1,21 @@
+clc ; clear ;
+
+syms R L J B;
+syms Kt Ke ; 
+
+%% mechanical parameters
+
+m = 18.8; % unit: kg
+l = 0.41367 / 2; % half of wheel separation
+r = 0.085; %wheel radius
+
+%% wheel motor parameters
+P = 30;
+ke = 0.31; 
+kt = 0.01; % to be determined
+J = 0.1; % to be determined
+B = 0.1; % to be determined
+L = 0.1; % to be determined
+R = 0.02;
+
+%sim('BLDCmotor_modeling')
