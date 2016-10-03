b31beab09f54e2a99c3a132d349c8f38d8e0a07c
WeberHsu (Shang Wei) <patric0123@gmail.com>
2016-10-04 07:24:47 +0800

    backup

--- andbot1dot2/andbot1dot2_modeling_param.m
+++ andbot1dot2/andbot1dot2_modeling_param.m
@@ -18,4 +18,20 @@ B = 0.1; % to be determined
 L = 0.1; % to be determined
 R = 0.02;
 
+num = [0 1.6095];
+den = [1 1.7857];
+P = tf(num,den,'InputDelay',0.4)
+Intg = tf(1,[1,0])
+Q =  tf(1,[1,0],'InputDelay',0.4)
+Qall = Intg - Q;
+P0 = tf(num,den);
+
+Pall = P * Qall;
+step(P0,'b',P,'r')
+bode(P,'r',P0,'b')
+bode(Pall,'r')
+
+
+
+
 %sim('BLDCmotor_modeling')
